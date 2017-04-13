150b39741effbfbb86f6454f41a7f533be393de2
wiltrahan <twilorip@gmail.com>
2017-04-13 18:20:27 -0400

    add comments

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,8 @@ var model = {
   houseMembers: []
 };
 
-
+//first call...gets members by zip, pushes into the stateRequest array,
+//calls render as the callback function
 function searchMembersByZip(query, callback){
 
   $.ajax({
@@ -25,7 +26,9 @@ function searchMembersByZip(query, callback){
 };
 
 
-
+//once info is gotten from the button click, the data is sent to the proPublica1 array
+//the id is then sent to the second propublica call
+//this call gets all members by district & state
 function proPublicaCallOne(district, state) {
 
   $.ajax({
@@ -48,7 +51,8 @@ function proPublicaCallOne(district, state) {
   });
 
 };
-
+//the second call gets the member id, pushes the data into proPublica2, then opens the modal
+//this call allows you to pinpoint a specific member by their id that was gotten from the first call
 function proPublicaCallTwo(member_id, callback){
   $.ajax({
     url: 'https://api.propublica.org/congress/v1/members/' + member_id + '.json',
@@ -71,7 +75,9 @@ function proPublicaCallTwo(member_id, callback){
 };
 
 
-
+//shows main page listing of members that it gets from the stateRequest array
+//pushes all members NOT in the senate (house only) into the house members array
+//gives each button a unique id to correspond to the representative in the array
 function render(){
 
   $("#section-browse-dems ul").empty();
@@ -101,7 +107,7 @@ function render(){
           console.log("other");
       }
     }
-
+//once button is clicked, district number, and state are sent to the first propublica api call
     $("#section-browse-dems").on('click', '.moreInfo', function(event) {
 
       district = this.id;
@@ -113,7 +119,8 @@ function render(){
     });
 };
 
-
+//the modal is opened, and gets all the needed info from the proPublica2 array
+//rendering it on the page.
 function openModal(){
   var name, nextElection, web, chamber, percent, party, phone, infoList;
 
